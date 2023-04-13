c0e53586abc31d438421175529032af856b505ee
Thanakorn Boonyasena <Thanakorn.Boonyasena@gmail.com>
2023-04-13 15:45:10 +0700

    update 0.3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.urlencoded ({extended: true}));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views' ));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
@@ -13,6 +13,20 @@ app.get('/', (req, res) => {
     res.render('index');
   });
 
+app.post('/newbie-send', (req, res) => {
+  const password = req.body;
+  console.log(password)
+  const passwordlist = Object.values(password);
+  console.log(passwordlist)
+  let status = ""
+     if (passwordlist.length == 6) {
+      status = "ผ่าน"
+      res.render('beginer');
+     } else {
+      status = "ไม่ผ่าน"
+      res.render('newbie',{status:status});
+     }
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
@@ -27,7 +41,8 @@ app.get('/play', (req, res) => {
   });
 
   app.get('/newbie', (req, res) => {
-    res.render('newbie');
+    const status = ""
+    res.render('newbie',{status:status});
   });
 
   app.get('/beginer', (req, res) => {
