ad17275ce4354ba47ecb1b6e02e446254aa467ef
Thanakorn Boonyasena <Thanakorn.Boonyasena@gmail.com>
2023-04-13 12:23:45 +0700

    Updata 0.2

--- index.js
+++ index.js
@@ -13,4 +13,37 @@ app.get('/', (req, res) => {
     res.render('index');
   });
 
+
+app.get('/news', (req, res) => {
+    res.render('news');
+  });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+app.get('/play', (req, res) => {
+    res.render('play');
+  });
+
+  app.get('/newbie', (req, res) => {
+    res.render('newbie');
+  });
+
+  app.get('/beginer', (req, res) => {
+    res.render('beginer');
+  });
+
+  app.get('/amateur', (req, res) => {
+    res.render('amateur');
+  });
+
+  app.get('/semi-pro', (req, res) => {
+    res.render('semi-pro');
+  });
+
+  app.get('/professional', (req, res) => {
+    res.render('professional');
+  });
+
 app.listen(8080 ,() => console.log("Server is running"))
